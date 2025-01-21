936b6dcd95a89bf80287557c699aaf90339519fd
Sara <sarasdj@stud.ntnu.no>
2025-01-21 12:17:00 +0100

    add: initial code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestDemo
+{
+    using System;
+
+    public class GeneralTree
+    {
+        class Node
+        {
+            
+        }
+    }
+
+    class TestDemo
+    {
+        // Main Method
+        static void Main(string[] args)
+        {
+            // "printing Hello World"
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
