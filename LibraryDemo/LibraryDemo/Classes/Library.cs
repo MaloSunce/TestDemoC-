956828e0f85ff4d1c2cc056cd6790fe430cc33bd
Sara <sarasdj@stud.ntnu.no>
2025-01-23 12:10:33 +0100

    update: add error handling to Library methods

--- LibraryDemo/LibraryDemo/Classes/Library.cs
+++ LibraryDemo/LibraryDemo/Classes/Library.cs
@@ -7,22 +7,30 @@ public class Library
 
     public void AddBook(string title, string author, string publisher, DateTime publicationDate)
     {
-        // Find unused bookID
-        var sortedBooks = AllBooks.OrderBy(book => book.BookId).ToList();
-        var lastId = sortedBooks[^1].BookId;
-
-        // Create new Book instance and push to AllBooks
-        var newBook = new Book()
+        try
         {
-            BookId = lastId + 1,
-            Title = title,
-            Author = author,
-            Publisher = publisher,
-            PublicationDate = publicationDate,
-            Available = true,
-            Lender = null,
-        };
-        AllBooks.Add(newBook);
+            // Find unused bookID
+            var sortedBooks = AllBooks.OrderBy(book => book.BookId).ToList();
+            var lastId = sortedBooks[^1].BookId;
+
+            // Create new Book instance and push to AllBooks
+            var newBook = new Book()
+            {
+                BookId = lastId + 1,
+                Title = title,
+                Author = author,
+                Publisher = publisher,
+                PublicationDate = publicationDate,
+                Available = true,
+                Lender = null,
+            };
+            AllBooks.Add(newBook);
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     public void RemoveBook(string title)
@@ -31,29 +39,46 @@ public class Library
 
     public void UpdateBookAvailability(int bookId, User? user, bool borrow)
     {
-        var book = AllBooks.FirstOrDefault(book => book.BookId == bookId);
-        
-        if (book != null)
+        try
         {
-            book.Lender = user;
-            book.Available = borrow;
+            var book = AllBooks.FirstOrDefault(book => book.BookId == bookId);
+
+            if (book != null)
+            {
+                book.Lender = user;
+                book.Available = borrow;
+            }
+
+            else
+            {
+                throw new Exception($"Failed to find the book with id {bookId}");
+            }
         }
-        else
+        catch (Exception e)
         {
-            throw new Exception($"Failed to find the book with id {bookId}");
+            Console.WriteLine(e);
+            throw;
         }
     }
 
     public void NewUser(string username)
     {
-        // Sort all users by UserId
-        var sortedUsers = AllUsers.OrderBy(user => user.UserId).ToList();
+        try
+        {
+            // Sort all users by UserId
+            var sortedUsers = AllUsers.OrderBy(user => user.UserId).ToList();
 
-        // Get largest Id
-        var lastId = sortedUsers[^1].UserId;
+            // Get largest Id
+            var lastId = sortedUsers[^1].UserId;
 
-        // Create new user and push to list
-        var newUser = new User(username, lastId + 1);
-        AllUsers.Add(newUser);
+            // Create new user and push to list
+            var newUser = new User(username, lastId + 1);
+            AllUsers.Add(newUser);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
